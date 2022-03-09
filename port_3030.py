2ceea86d9c0296bb5a6777368dddf2348f5efb59
AlanWarburton <58405195+AlanWarburton@users.noreply.github.com>
2022-03-09 20:43:19 -0300

    Update port_3030.py

--- port_3030.py
+++ port_3030.py
@@ -39,8 +39,55 @@ FAMILY = "VictoryGate.A"
 # whatever reason this is not possible, set the variable PUBLIC_IP manually. For logging purposes only.
 
 
-class MultiThreading:
-    def __init__(self, ip, port):
+class Helpers:  
+    @staticmethod
+    def stringToHex(unicode: str) -> bytes:
+        bytes = bytearray(unicode)
+        hex = binascii.hexlify(bytes)
+        return hex
+
+    @staticmethod
+    def prohibitNewConn(ip_addr: str, mutex: threading.Lock) -> None:
+        """Block an IP address on the local firewall (iptables)."""
+        try:
+            p = subprocess.Popen(["sudo", "iptables", "-w", "-A", "INPUT", "-s", ip_addr, "-j", "DROP"], stdout=subprocess.PIPE)
+            p.communicate()
+            mutex.release()
+        except:
+            mutex.release()
+
+    @staticmethod
+    def validateBuffer(recv_buffer: bytes) -> bool:
+        """Check if the received buffer matches the malware signature."""
+        received = Helpers.stringToHex(recv_buffer)
+        if received.startswith(SIGNATURE):
+            return True
+        return False
+    
+    @staticmethod
+    def logger(raddr: str, rport: int, lport: int) -> None:
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+        columns = "timestamp", "ip", "port", "cc_ip", "cc_port", "infection"
+        mutex = Lock()
+        if not os.path.isfile(LOGPATH):
+            with open(LOGPATH, 'a+') as write_obj:
+                csv_writer = csv.writer(write_obj)
+                csv_writer.writerow(columns)
+        with mutex:
+            try:
+                time.sleep(0.035)
+                if os.path.isfile(LOGPATH):
+                    with open(LOGPATH, "a") as log:
+                        line = ','.join(str(x) for x in (timestamp, raddr, rport, PUBLIC_IP, lport, FAMILY))
+                        log.write(line + "\n")
+                        log.close()
+                        Helpers.prohibitNewConn(raddr, mutex)
+            except:
+                mutex.release()
+    
+    
+class Server:
+    def __init__(self, ip: str, port: int):
         self.ip = ip
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,58 +101,15 @@ class MultiThreading:
             client.settimeout(20)
             threading.Thread(target = self.newClient, args = (client, ip)).start()
 
-    def logger(self, raddr, rport):
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-        columns = "timestamp", "ip", "port", "cc_ip", "cc_port", "infection"
-        localPort = self.port
-        localAddr = PUBLIC_IP
-        mutex = Lock()
-        if not os.path.isfile(LOGPATH):
-            with open(LOGPATH, 'a+') as write_obj:
-                csv_writer = csv.writer(write_obj)
-                csv_writer.writerow(columns)
-        with mutex:
-            try:
-                time.sleep(0.035)
-                if os.path.isfile(LOGPATH):
-                    with open(LOGPATH, "a") as log:
-                        line = ','.join(str(x) for x in (timestamp, raddr, rport, localAddr, localPort, FAMILY))
-                        log.write(line + "\n")
-                        log.close()
-                        self.prohibitNewConn(raddr, mutex)
-            except:
-                mutex.release()
-
-    @staticmethod
-    def stringToHex(unicode):
-        bytes = bytearray(unicode)
-        hex = binascii.hexlify(bytes)
-        return hex
-
-    def validateBuffer(self, recv_buffer):
-        received = self.stringToHex(recv_buffer)
-        if received.startswith(SIGNATURE):
-            return True
-        return False
-
-    @staticmethod
-    def prohibitNewConn(ip_addr, mutex):
-        try:
-            p = subprocess.Popen(["sudo", "iptables", "-w", "-A", "INPUT", "-s", ip_addr, "-j", "DROP"], stdout=subprocess.PIPE)
-            p.communicate()
-            mutex.release()
-        except:
-            mutex.release()
-
-    def newClient(self, client, ip):
+    def newClient(self, client, ip: tuple):
         while True:
             try:
                 msg = client.recv(BUFFER)
-                if self.validateBuffer(msg):
+                if Herlpers.validateBuffer(msg):
                     client_ip, client_port = ip
-                    self.logger(client_ip, client_port)
+                    Helpers.logger(client_ip, client_port, self.port)
                 else:
-                    self.prohibitNewConn(client_ip)
+                    Helpers.prohibitNewConn(client_ip)
                     return False
             except:
                 return False
@@ -118,12 +122,13 @@ if __name__ == "__main__":
     try:
         url = "https://ipinfo.io/json"
         response = urllib2.urlopen(url)
-        data = json.load(response)
-        PUBLIC_IP = data['ip']
-        MultiThreading(TCP_IP, TCP_PORT).listen()
+        PUBLIC_IP = json.load(response).get("ip")
+        Server(TCP_IP, TCP_PORT).listen()
+        
     except KeyboardInterrupt:
         print ("\nShutting down server on user interrupt... Wait for all threads to finalize")
         raise SystemExit()
+        
     except Exception as e:
         print ("\n[!] Unexpected error:\n")
         print (e)
